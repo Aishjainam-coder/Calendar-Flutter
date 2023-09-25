eab0e1af649b85a1c34cdaafc902a41878fc08da
Aishwarya  jain <jainaish.coder@gmail.com>
2023-09-25 07:16:14 +0530

    secomd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar_null_safe/table_calendar_null_safe.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,165 +11,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Aishwarya App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Portfolio"),
-        ),
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                margin: const EdgeInsets.only(top: 30),
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(200),
-                  color: Colors.lightBlueAccent,
-                  border: Border.all(color: Colors.black, width: 2),
-                ),
-                child: const Center(
-                  child: Text(
-                    "A",
-                    style: TextStyle(fontSize: 60, color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(40),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Aishwarya Jain",
-                    icon: Icon(
-                      Icons.person,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 30, right: 30),
-              decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Password",
-                    icon: Icon(
-                      Icons.lock_clock_rounded,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 30, top: 30, right: 30),
-              decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.only(left: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Github",
-                    icon: Icon(
-                      Icons.link,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 30, top: 30, right: 30),
-              height: 50,
-              width: 500,
-              decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const Center(
-                child: Text(
-                  "Update",
-                  style: TextStyle(color: Colors.amberAccent),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            //FlatButton
-            TextButton(
-              onPressed: () {
-                // Handle onPressed event here
-              },
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.lightGreenAccent,
-                shadowColor: Colors.white,
-              ),
-              child: const Text(
-                "Update",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Handle onPressed event here
-              },
-              style: ButtonStyle(
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                overlayColor:
-                    MaterialStateProperty.all<Color>(Colors.greenAccent),
-              ),
-              child: const Text(
-                "Update",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            FloatingActionButton(
-  foregroundColor: Colors.black,
-  backgroundColor: Colors.yellow,
-  onPressed: () {
-    // Handle onPressed event here
-  },
-  child: const Icon(Icons.save),
-),
+      home: Calendar(),
+    );
+  }
+}
 
-FloatingActionButton.extended(
-  onPressed: () {
-    // Handle onPressed event here
-  },
-  label: const Text("Save"),
-  icon: const Icon(Icons.save),
-  backgroundColor: Colors.yellow,
-  foregroundColor: Colors.black,
-),
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
 
+class _CalendarState extends State<Calendar> {
+  CalendarController _controller = CalendarController();
 
-
-
-
-
-          ],
-        ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calendar"),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            initialCalendarFormat: CalendarFormat.month,
+            calendarController: _controller,
+            calendarStyle: const CalendarStyle(
+              todayStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              todayColor: Colors.blueAccent,
+              selectedColor: Colors.green,
+            ),
+            headerStyle: HeaderStyle(
+              centerHeaderTitle: true,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              formatButtonTextStyle: const TextStyle(color: Colors.white),
+              formatButtonShowsNext: false,
+            ),
+          ),
+        ],
       ),
     );
   }
